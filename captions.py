f593c1a86bc9742de33a9ff2c33987478c3880d1
Aaron <aaron.philipp@uni-potsdam.de>
2022-11-18 10:55:32 +0100

    how to save the captions in a txt-file

--- captions.py
+++ captions.py
@@ -1,14 +1,12 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from main import video_id
 
 text = []
-
-video_id = 'tbapalw2-Eo'
 
 caption = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=['de'])
 
 for i in caption:
-    outtxt = (i['text'])
-    text.append(outtxt)
+    tmp = (i['text'])
 
-    with open('op.txt', "a") as opf:
-        opf.write(outtxt + '\n')
+    with open(str(video_id) + '.txt', "a") as opf:
+        opf.write(tmp + '\n')
